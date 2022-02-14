52cadd5f07a53ff167df68d9b62b8bc16cfcd75d
mitochon9 <mitochon9@gmail.com>
2022-02-14 15:07:59 +0900

    ディレクトリ構成の変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Layout } from "src/layout/Layout";
+import { Index } from "src/pages/index";
 
 export default function Home() {
   return (
     <Layout>
-      <div></div>
+      <Index />
     </Layout>
   );
 }
