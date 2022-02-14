71b07898f33bd2b947e4cb652e3682908302cc1f
kensiiwasaki <kensiiwasaki@gmail.com>
2022-02-14 20:35:45 +0900

    練習用

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,9 @@ import { Layout } from "src/layouts/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div></div>
+      <div>
+        <h1>練習用</h1>
+      </div>
     </Layout>
   );
 }
