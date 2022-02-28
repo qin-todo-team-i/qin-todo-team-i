75e259afb3cb7a1778a9eb51296e2c6e4f6ed237
soeda <soeda.yuki@gmail.com>
2022-02-28 11:50:48 +0900

    アカウントページのデザイン修正

--- src/components/Account/DrawerItem.jsx
+++ src/components/Account/DrawerItem.jsx
@@ -4,10 +4,10 @@ import { ChevronRightIcon } from '@heroicons/react/outline';
 
 const DrawerItem = ({ item, selectedItem, path }) => (
 	<>
-		<li className='mt-6 flex justify-between'>
+		<li className='mt-6 flex justify-between items-center'>
 			<span>{item}</span>
-			<span className='flex gap-x-10'>
-				{selectedItem ? <span className=''>{selectedItem}</span> : null}
+			<span className='flex items-center'>
+				{selectedItem ? <span className='pr-4'>{selectedItem}</span> : null}
 				<span>
 					<Link href={path}>
 						<ChevronRightIcon className='w-5 h-5 text-gray-400' />
--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import React from 'react';
 import Drawer from 'src/components/Account/Drawer';
-import { XIcon } from '@heroicons/react/solid';
+import { XIcon, CogIcon, LogoutIcon } from '@heroicons/react/solid';
+import { Layout } from 'src/layouts/Layout';
 
 const Account = () => {
 	const drawerMenuContents = [
@@ -28,30 +29,52 @@ const Account = () => {
 	];
 
 	return (
-		<div className='px-8 py-4'>
-			<div className='flex justify-between justify-items-center'>
-				<span className='flex flex-col justify-center'>
-					<XIcon className='w-5 h-5' />
-				</span>
-				<h1 className='text-lg'>アカウント</h1>
-				<span></span>
+		<Layout>
+			<div className='px-8 relative'>
+				<div className='py-4 max-w-2xl mx-auto'>
+					<div className='flex justify-between justify-items-center lg:hidden'>
+						<span className='flex flex-col justify-center'>
+							<XIcon className='w-5 h-5' />
+						</span>
+						<h1 className='text-lg'>アカウント</h1>
+						<span></span>
+					</div>
+					{drawerMenuContents.map((drawerMenuContent) => {
+						return (
+							<>
+								<Drawer menuContent={drawerMenuContent} />
+							</>
+						);
+					})}
+					<ul className='list-none font-bold relative'>
+						<li className='mt-6'>
+							<Link href='/'>お問い合わせ</Link>
+						</li>
+						<li className='mt-6 flex justify-between'>
+							<span>バージョン</span>
+							<span>1.0.0</span>
+						</li>
+					</ul>
+				</div>
+
+				<ul className='hidden lg:flex absolute top-0 right-0 w-48 py-1 flex-col border rounded-xl drop-shadow-sm'>
+					<Link href='/'>
+						<li className='flex items-center gap-x-3 px-4 py-2 hover:bg-gray-100'>
+							<span>
+								<CogIcon className='w-5 h-5' />
+							</span>
+							<span>設定</span>
+						</li>
+					</Link>
+					<li className='flex items-center gap-x-3 px-4 py-2 text-red-500 hover:bg-gray-100'>
+						<span>
+							<LogoutIcon className='w-5 h-5' />
+						</span>
+						<span>ログアウト</span>
+					</li>
+				</ul>
 			</div>
-			{drawerMenuContents.map((drawerMenuContent) => {
-				return (
-					<>
-						<Drawer menuContent={drawerMenuContent} />
-					</>
-				);
-			})}
-			<ul className='list-none font-bold relative'>
-				<li className='mt-6'>
-					<Link href='/'>お問い合わせ</Link>
-				</li>
-				<li className='mt-6'>
-					バージョン<span className='absolute right-0'>1.0.0</span>
-				</li>
-			</ul>
-		</div>
+		</Layout>
 	);
 };
 
