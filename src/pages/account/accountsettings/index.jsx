3b96f4d0c2361666440aac73d6324816719b0976
shota <morgen.ist.auch.noch.ein.tag@gmail.com>
2022-03-02 15:55:08 +0900

    アカウント連携ページモック作成

---
+++ src/pages/account/accountsettings/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
+
+import { Layout } from "src/layouts/Layout";
+
+const AccountSettings = () => {
+  return (
+    <Layout>
+      <div className="max-w-2xl mx-auto px-6">
+        <div className="lg:hidden flex items-center relative justify-between gap-x-6 mb-8">
+          <ChevronLeftIcon className="w-6 h-6" />
+          <h1 className="text-lg font-bold">アカウントの連携</h1>
+          <span></span>
+        </div>
+        <div className="hidden lg:block">
+          <ul className="flex items-center gap-x-8 text-sm font-bold mb-4">
+            <li>ホーム</li>
+            <ChevronRightIcon className="w-4 h-4 text-gray-400" />
+            <li>アカウントの連携</li>
+          </ul>
+          <h1 className="text-3xl font-bold mb-8">アカウントの連携</h1>
+        </div>
+
+        <section className="mb-14">
+          <ul className="">
+            <li className="flex justify-between items-center mb-3">
+              <span className="inline-flex items-center gap-3">
+                <Image src="/icon_google.svg" alt="logo" width={24} height={24} />
+                <span>Goole</span>
+              </span>
+              <button className="block px-6 py-2 bg-slate-100 rounded-3xl text-black">解除する</button>
+            </li>
+            <li className="flex justify-between items-center">
+              <span className="inline-flex items-center gap-3">
+                <Image src="/icon_apple.svg" alt="logo" width={24} height={24} />
+                <span>Apple</span>
+              </span>
+              <button className="block px-6 py-2 bg-blue-600 rounded-3xl text-white">連携する</button>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2 className="text-slate-200 font-bold mb-5">アカウントの操作</h2>
+          <ul>
+            <li className="mb-5">
+              <span className="text-red-500 font-bold cursor-pointer">ログアウト</span>
+            </li>
+            <li className="">
+              <span className="text-red-500 font-bold cursor-pointer">アカウントの削除</span>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </Layout>
+  );
+};
+
+export default AccountSettings;
