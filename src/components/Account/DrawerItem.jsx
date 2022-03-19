cf1c8a9482118a4bddd12cf87dae2eb59f45d284
syu-dev07 <mutuyakumo7@docomo.ne.jp>
2022-03-20 03:21:54 +0900

    クリック範囲の調整・カーソルポインター修正・メニューバーコメントアウト

--- src/components/Account/DrawerItem.jsx
+++ src/components/Account/DrawerItem.jsx
@@ -4,17 +4,17 @@ import { ChevronRightIcon } from "@heroicons/react/outline";
 
 const DrawerItem = ({ item, selectedItem, path }) => (
   <>
-    <li className="mt-6 flex justify-between items-center">
-      <span>{item}</span>
-      <span className="flex items-center">
-        {selectedItem ? <span className="pr-4">{selectedItem}</span> : null}
-        <span>
-          <Link href={path}>
+    <Link href={path}>
+      <li className="mt-6 flex justify-between items-center cursor-pointer">
+        <span>{item}</span>
+        <span className="flex items-center">
+          {selectedItem ? <span className="pr-4">{selectedItem}</span> : null}
+          <span>
             <ChevronRightIcon className="w-5 h-5 text-gray" />
-          </Link>
+          </span>
         </span>
-      </span>
-    </li>
+      </li>
+    </Link>
   </>
 );
 
