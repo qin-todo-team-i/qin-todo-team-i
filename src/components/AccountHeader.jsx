fa216332118699c77f21ecffe953e6353d361fb6
shota <morgen.ist.auch.noch.ein.tag@gmail.com>
2022-03-21 23:39:54 +0900

    ✨ 設定画面のヘッダー戻るアイコンを出し分ける処理を追加

--- src/components/AccountHeader.jsx
+++ src/components/AccountHeader.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { ChevronLeftIcon } from "@heroicons/react/solid";
+import { ChevronLeftIcon, XIcon } from "@heroicons/react/solid";
 
 export const AccountHeader = (props) => {
-  const { title } = props;
+  const { title, ...rest } = props;
   const router = useRouter();
 
   return (
     <>
       <div className="max-w-5xl mx-auto flex justify-between items-center h-14 relative gap-x-6 px-6">
         <button onClick={router.back}>
-          <ChevronLeftIcon className="w-6 h-6" />
+          {rest.isTopLevel ? <XIcon className="w-6 h-6" /> : <ChevronLeftIcon className="w-6 h-6" />}
         </button>
 
         <h1 className="text-lg font-bold">{title}</h1>
