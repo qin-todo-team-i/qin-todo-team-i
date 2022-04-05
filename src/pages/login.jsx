5ff444f1d85ae123ee04f7186f1721241a9b0d0d
けんし <kensiiwasaki@gmail.com>
2022-04-05 19:52:57 +0900

    ログイン画面の作成

---
+++ src/pages/login.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="h-screen w-screen flex flex-col justify-center items-center px-7">
+        <img src="/logo.png" alt="logo" />
+        <button className="block w-full h-14 mt-9 bg-blue-600 rounded-3xl text-white max-w-xs">
+          <Link href="/">
+            <a>ログイン</a>
+          </Link>
+        </button>
+      </div>
+    </>
+  );
+};
+export default Login;
