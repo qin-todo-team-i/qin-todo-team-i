69146549e3218fe679211ce7d12bc926d113486e
けんし <kensiiwasaki@gmail.com>
2022-04-17 17:48:41 +0900

    login画面の利用

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
-const Login = () => {
+const Login = (toggleStatus) => {
   return (
     <>
       <div className="h-screen w-screen flex flex-col justify-center items-center px-7">
         <img src="/logo.png" alt="logo" />
-        <button className="block w-full h-14 mt-9 bg-blue-600 rounded-3xl text-white max-w-xs">
+        <button
+          onClick={toggleStatus}
+          className="block w-full h-14 mt-9 bg-blue-600 rounded-3xl text-white max-w-xs"
+        >
           <Link href="/">
             <a>ログイン</a>
           </Link>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,61 +1,32 @@
-import React, { useCallback, useEffect } from "react";
-import { useRecoilState } from "recoil";
-import { Tasks } from "src/components/Tasks";
-import { useFetch } from "src/hooks/useFetch";
-import useGroupedItems from "src/hooks/useGroupedItems";
-import { useTaskApi } from "src/hooks/useTaskApi";
-import { Layout } from "src/layouts/Layout";
-import { tasksUrl } from "src/lib/TasksUrl";
-import { tasksState } from "src/state/TasksState";
+import Link from "next/link";
+import React, { useState } from "react";
+import Home from "src/components/Home";
+import Login from "src/components/login";
 
-const Home = () => {
-  const [tasks, setTasks] = useRecoilState(tasksState);
-
-  const { getTasks } = useTaskApi();
-  useEffect(() => {
-    getTasks(setTasks);
-  }, []);
-
-  const [groupedItems, items, setItems] = useGroupedItems(tasks);
-
-  const moveItem = useCallback(
-    (dragIndex, targetIndex, group) => {
-      const item = items[dragIndex];
-      if (!item) return;
-      setItems((prevState) => {
-        const newItems = prevState.filter((_, idx) => idx !== dragIndex);
-        newItems.splice(targetIndex, 0, { ...item, group });
-        return newItems;
-      });
-    },
-    [items, setItems]
-  );
-
-  const { error, isLoading } = useFetch(tasksUrl);
-  if (isLoading) {
-    return <div>now ...Loading</div>;
-  }
-  if (error) {
-    return <div>データ取得に失敗しました</div>;
-  }
-
-  const todayItems = groupedItems["today"];
-  const tomorrowItems = groupedItems["tomorrow"];
-  const nextTimeItems = groupedItems["nextTime"];
-
-  const firstIndexToday = 0;
-  const firstIndexTomorrow = todayItems.length;
-  const firstIndexNextTime = firstIndexTomorrow + tomorrowItems.length;
+const page = () => {
+  const [userLogin, setUserLogin] = useState(false);
 
   return (
-    <Layout>
-      <div className="flex justify-between max-w-7xl mt-10 mx-auto">
-        <Tasks limit="today" items={todayItems} firstIndex={firstIndexToday} onMove={moveItem} />
-        <Tasks limit="tomorrow" items={tomorrowItems} firstIndex={firstIndexTomorrow} onMove={moveItem} />
-        <Tasks limit="nextTime" items={nextTimeItems} firstIndex={firstIndexNextTime} onMove={moveItem} />
-      </div>
-    </Layout>
+    <>
+      {userLogin ? (
+        <Home />
+      ) : (
+        <>
+          <div className="h-screen w-screen flex flex-col justify-center items-center px-7">
+            <img src="/logo.png" alt="logo" />
+            <button
+              onClick={() => setUserLogin(!userLogin)}
+              className="block w-full h-14 mt-9 bg-blue-600 rounded-3xl text-white max-w-xs"
+            >
+              <Link href="/">
+                <a>ログイン</a>
+              </Link>
+            </button>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
-export default Home;
+export default page;
